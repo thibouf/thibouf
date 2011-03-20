1802fe2bc6bad47afbb4a42a04db23d261f07323
Thibouf <thibouf@thibouf-linux-desktop.(none)>
2011-03-21 00:36:34 +0100

    Some fixes, scroll

---
+++ bubblelove/levels/lvl1.lua
@@ -0,0 +1,71 @@
+local Level =
+{
+	 
+	{
+		T = "Wall",
+		x = 650/2,
+		y = 625,
+		h = 650,
+		w = 50,
+	},
+	{
+		T = "Wall",
+		x = 650/2,
+		y = 25,
+		h = 650,
+		w = 50,
+	},
+	 
+	{
+		T = "Wall",
+		x = 25,
+		y = 650/2,
+		h = 50,
+		w = 650,
+	},
+	{
+		T = "Wall",
+		x = 625,
+		y = 650/2,
+		h = 50,
+		w = 650,
+	},
+	{
+		T = "Bubble",
+		x = 650/2,
+		y = 100,
+		color = "Special",
+		mass = 0,
+	},
+	{
+		T = "Bubble",
+		x = 650/2,
+		y = 80,
+		color = "Red",
+	},	 
+	{
+		T = "Bubble",
+		x = 650/2,
+		y = 110,
+		color = "Red",
+	},	
+	{
+		T = "Bubble",
+		x = 650/2 + 10,
+		y = 110,
+		color = "Red",
+	},	
+	{
+		T = "Vessel",
+		x = 650/2,
+		y = 650/2 + 50,
+		color = "Special",
+	},
+	{
+		T = "Tower",
+		x = 60,
+		y = 60,
+		target = 5,
+	}
+}
+return Level
